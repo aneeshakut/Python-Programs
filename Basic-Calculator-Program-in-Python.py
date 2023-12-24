23f5b32fce7e471b76626a3fc40825b225fdee5d
Aneesh Ajay Akut <135816561+aneeshakut@users.noreply.github.com>
2023-12-24 17:03:35 +0530

    Update Basic-Calculator-Program-in-Python.py

--- Basic-Calculator-Program-in-Python.py
+++ Basic-Calculator-Program-in-Python.py
@@ -1,47 +1,66 @@
 #Basic Calculator Program in Python
 
-#Operator Functions
+import math
 
-#Addition
-def add(x,y):
-    return x+y
-#Subtraction
-def sub(x,y):
-    return x-y
-#Multiplication
-def mul(x,y):
-    return x*y
-#Division
-def div(x,y):
-    if(y!=0):
-        return x/y
-    else:
-        print("Mathematical error!")
-        print("Run the program again!")
-#Power
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+#This function raises the number to the power typed
 def pow(x,y):
-    return x**y
+    return math.pow(x,y)
 
-#User Input
-num1=float(input("Enter first number:"))
-print("Codes of operators:add-1,sub-2,mul-3,div-4,pow-5")
-op=int(input("Enter operator:"))
-num2=float(input("Enter second number:"))
-ans=0
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Power")
 
-#Calculation Functions
-if(op==1):
-    ans=add(num1,num2)
-elif(op==2):
-    ans=sub(num1,num2)
-elif(op==3):
-    ans=mul(num1,num2)
-elif(op==4):
-    ans=div(num1,num2)
-elif(op==5):
-    ans=pow(num1,num2)
-else:
-    print("Invalid syntax!Run the program again!")
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4/5): ")
 
-#Displaying the answer
-print("Answer= " , ans)
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4' , '5'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+        elif choice == '5':
+            print(num1, "^", num2, "=", pow(num1, num2))
+
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+            break
+    else:
+        print("Invalid Input")
